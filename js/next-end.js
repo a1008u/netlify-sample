6e53b92f2610e2d0672b989c80d3d649734ea56b
akira uemoto <empty.aauu0001@gmail.com>
2019-06-16 23:32:33 +0900

    構成変更

--- js/next-end.js
+++ js/next-end.js
@@ -1,20 +1,79 @@
-window.addEventListener("load", (event) => {
+window.addEventListener("load", async (event) => {
 
   const name = document.getElementById("name");
   const email = document.getElementById("email");
   const role = document.getElementById("role");
   const message = document.getElementById("message");
 
+  url = ""
+  tmpJson = await getTmpJson(url, {"name":sessionStorage.getItem('name')})
+  console.log(tmpJson)
 
-  name.textContent = sessionStorage.getItem('name')
-  email.textContent = sessionStorage.getItem('email')
-  role.textContent = sessionStorage.getItem('role')
-  message.textContent = sessionStorage.getItem('message')
-
-  console.log(name.value, email.value, role.value, message.value)
+  name.textContent = tmpJson.name
+  email.textContent = tmpJson.email
+  role.textContent = tmpJson.role
+  message.textContent = tmpJson.message
 })
 
 
-document.getElementById("button-end").addEventListener("click",()=>{
-  window.location.href = '../html/thank.html';
+document.getElementById("button-end").addEventListener("click", async ()=>{
+
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const role = document.getElementById("role");
+  const message = document.getElementById("message");
+
+  url = ""
+  jsonC = {"name": name.textContent, 'email': email.textContent, 'role': role.textContent, 'message': message.textContent}
+  await setInfo(url, jsonC)
+
+  sessionStorage.removeItem('name')
+
+  window.location.href = '../input-cloud/thank.html';
 })
+
+document.getElementById("button-back").addEventListener("click", async ()=>{
+
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const role = document.getElementById("role");
+  const message = document.getElementById("message");
+
+  url = ""
+  jsonC = {"name": name.textContent, 'email': email.textContent, 'role': role.textContent, 'message': message.textContent}
+  console.log(jsonC)
+  await setInfo(url, jsonC)
+  sessionStorage.removeItem('name')
+  window.location.href = '../input-cloud/input.html';
+})
+
+async function getTmpJson(request, nameJson){
+  const method = "POST";
+  const body = JSON.stringify(nameJson);
+  const headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+  };
+
+  return new Promise(resolve => {
+      fetch(request, {method, headers, body})
+      .then(response => response.json())
+      .then(body => resolve(body));
+  });
+}
+
+
+async function setInfo(request, formInfo) {
+  const method = "POST";
+  const body = JSON.stringify(formInfo);
+  const headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+  };
+
+  return new Promise(resolve => {
+      fetch(request, {method, headers, body})
+      .then(response => response.json())
+      .then(body => resolve(body));
+  });
+};
--- js/next.js
+++ js/next.js
@@ -6,8 +6,11 @@ document.getElementById("button-confirm").addEventListener("click",async ()=>{
   const message = document.getElementById("message");
 
   if (name.validity.valid && email.validity.valid && role.validity.valid && message.validity.valid) {
-    await set()
-    window.location.href = '../html/confirm.html';
+    url = ""
+    jsonC = {"name": name.value, 'email': email.value, 'role': role.value,'message': message.value}
+    await getFormValid(url, jsonC)
+    await set(name.value)
+    window.location.href = '../input-cloud/confirm.html';
   }
 
   console.log(name.validity)
@@ -15,13 +18,22 @@ document.getElementById("button-confirm").addEventListener("click",async ()=>{
 
 })
 
-async function set(){
-  const name = document.getElementById("name");
-  const email = document.getElementById("email");
-  const role = document.getElementById("role");
-  const message = document.getElementById("message");
-  sessionStorage.setItem('name', name.value);
-  sessionStorage.setItem('email', email.value);
-  sessionStorage.setItem('role', role.value);
-  sessionStorage.setItem('message', message.value);
+async function set(name){
+  sessionStorage.setItem('name', name);
 }
+
+async function getFormValid(request, formInfo) {
+  const method = "POST";
+  const body = JSON.stringify(formInfo);
+  const headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+  };
+
+  return new Promise(resolve => {
+      fetch(request, {method, headers, body})
+      .then(response => response.json())
+      .then(body => resolve(body));
+  });
+};
+
