c48b639cbbbd368b843ae2434168753fe3c9df1e
akira uemoto <empty.aauu0001@gmail.com>
2019-06-18 22:05:15 +0900

    修正

--- js/input-netlify/next-end.js
+++ js/input-netlify/next-end.js
@@ -1,17 +1,27 @@
 window.addEventListener("load", async (event) => {
 
-  const name = document.getElementById("name");
-  const email = document.getElementById("email");
-  const role = document.getElementById("role");
-  const message = document.getElementById("message");
-  name.value = sessionStorage.getItem("name")
-  email.value = sessionStorage.getItem("email")
-  role.value = sessionStorage.getItem("role")
-  message.value = sessionStorage.getItem("message")
-  name.disabled = true;
-  email.disabled = true;
-  role.disabled = true;
-  message.disabled = true;
+  const form = document.forms.contact;
+  form.name.value = sessionStorage.getItem("name")
+  form.email.value = sessionStorage.getItem("email")
+  form.role.value = sessionStorage.getItem("role")
+  form.message.value = sessionStorage.getItem("message")
+  form.name.disabled = true;
+  form.email.disabled = true;
+  form.role.disabled = true;
+  form.message.disabled = true;
+
+  // const name = document.getElementById("name");
+  // const email = document.getElementById("email");
+  // const role = document.getElementById("role");
+  // const message = document.getElementById("message");
+  // name.value = sessionStorage.getItem("name")
+  // email.value = sessionStorage.getItem("email")
+  // role.value = sessionStorage.getItem("role")
+  // message.value = sessionStorage.getItem("message")
+  // name.disabled = true;
+  // email.disabled = true;
+  // role.disabled = true;
+  // message.disabled = true;
 })
 
 document.getElementById("button-back").addEventListener("click", async ()=>{
